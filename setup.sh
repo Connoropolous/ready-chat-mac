2e13813182ba0220400c5455dc558901cf5cb2a4
Connor Turland <connorturland@gmail.com>
2019-03-27 21:34:27 -0400

    update address of live node

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${DIR}"
-./driver wss://99.224.95.144:5555/?a=HcSci3EmBbcFkzc5d3SiDOxooCmo47qj39oSv4H4vMGT4xow5ybXq7bq73C53hr
+./driver wss://99.224.95.144:5555/?a=HcSCJR5o6u4PDYpfsxPJ5ORBG7Q74cuy5hMSdD3ERv3humbxeFusqOw3Xttyaxi
