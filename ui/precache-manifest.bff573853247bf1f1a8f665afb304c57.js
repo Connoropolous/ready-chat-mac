54b24ad6a888a756494ebd82d797fe1c3311a0b2
Connor Turland <connorturland@gmail.com>
2019-04-03 13:45:31 -0400

    update ui to work with 009

--- ui/precache-manifest.bff573853247bf1f1a8f665afb304c57.js
+++ ui/precache-manifest.bff573853247bf1f1a8f665afb304c57.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "01fb93720043ceeaca12",
-    "url": "/static/js/main.e0c43bad.chunk.js"
+    "revision": "fc595f26c48f4b28f9b6",
+    "url": "/static/js/main.85b5b6e2.chunk.js"
   },
   {
     "revision": "035dd9e00b7c238dfc70",
     "url": "/static/js/2.fa93c988.chunk.js"
   },
   {
-    "revision": "01fb93720043ceeaca12",
+    "revision": "fc595f26c48f4b28f9b6",
     "url": "/static/css/main.448ce91d.chunk.css"
   },
   {
-    "revision": "c69abf48d22eee7beac25360f63ac5ea",
+    "revision": "b50cce7227b89e9d4c04a6a99f1aaaf0",
     "url": "/index.html"
   }
 ];
